d72daac03609fae94e6ed8ce94684aea001f1078
Marlon Esteban Linares Zambrano <47485005+marlonlinares16@users.noreply.github.com>
2021-09-26 07:09:41 -0500

    Authentication working but dont work the deploy

---
+++ service_authentication/AuthenticationService/Controllers/AuthenticationsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AuthenticationService.Data;
+using Domain_Model_App;
+
+namespace AuthenticationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationsController : ControllerBase
+    {
+        private readonly AuthenticationServiceContext _context;
+
+        public AuthenticationsController(AuthenticationServiceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Authentications
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Authentication>>> GetAuthentication()
+        {
+            return await _context.Authentication.ToListAsync();
+        }
+
+        // GET: api/Authentications/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Authentication>> GetAuthentication(int id)
+        {
+            var authentication = await _context.Authentication.FindAsync(id);
+
+            if (authentication == null)
+            {
+                return NotFound();
+            }
+
+            return authentication;
+        }
+
+        // PUT: api/Authentications/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAuthentication(int id, Authentication authentication)
+        {
+            if (id != authentication.AuthenticationID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(authentication).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AuthenticationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Authentications
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Authentication>> PostAuthentication(Authentication authentication)
+        {
+            authentication.EncryptPassword(authentication.Password);
+            _context.Authentication.Add(authentication);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAuthentication", new { id = authentication.AuthenticationID }, authentication);
+        }
+
+        // DELETE: api/Authentications/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAuthentication(int id)
+        {
+            var authentication = await _context.Authentication.FindAsync(id);
+            if (authentication == null)
+            {
+                return NotFound();
+            }
+
+            _context.Authentication.Remove(authentication);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AuthenticationExists(int id)
+        {
+            return _context.Authentication.Any(e => e.AuthenticationID == id);
+        }
+    }
+}
---
+++ service_authentication/AuthenticationService/Data/AuthenticationServiceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Domain_Model_App;
+
+namespace AuthenticationService.Data
+{
+    public class AuthenticationServiceContext : DbContext
+    {
+        public AuthenticationServiceContext (DbContextOptions<AuthenticationServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Domain_Model_App.Authentication> Authentication { get; set; }
+    }
+}
